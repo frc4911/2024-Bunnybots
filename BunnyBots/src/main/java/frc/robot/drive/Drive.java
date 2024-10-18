0a89b0099feac3c8f4d2087241b4cbc3c6518b54
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-10-17 19:40:00 -0700

    Added Drive files

--- BunnyBots/src/main/java/frc/robot/drive/Drive.java
+++ BunnyBots/src/main/java/frc/robot/drive/Drive.java
@@ -2,16 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.drive;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public final class DriveSubsystem extends SubsystemBase {
-
+public final class Drive extends SubsystemBase {
+    // private final DriveConstants driveConstants;
     // TODO: declare motor controller variables here
 
-    public DriveSubsystem() {
+    public Drive() {
         // TODO: instantiate motor controllers here
     }
     
---
+++ BunnyBots/src/main/java/frc/robot/drive/DriveIO.java
@@ -0,0 +1,10 @@
+package frc.robot.drive;
+
+public interface DriveIO {
+    public static class  DriveIOInputs {
+    public double drivePositionRad = 0.0;
+    public double driveVelocityRadPerSec = 0.0;
+    public double driveAppliedVolts = 0.0; 
+        
+    }
+}
