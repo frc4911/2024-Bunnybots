786b5aec29358b6834915b0d16d0a6968a02cb78
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-10-24 19:05:13 -0700

    AdvantageKit works and Drive is a wip
    added 💩 n'stuff
    (had to restart my comp bcause vscode was bein funky)

--- BunnyBots/src/main/java/frc/robot/drive/Drive.java
+++ BunnyBots/src/main/java/frc/robot/drive/Drive.java
@@ -4,12 +4,80 @@
 
 package frc.robot.drive;
 
+import com.revrobotics.SparkAnalogSensor.Mode;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Constants;
 
 public final class Drive extends SubsystemBase {
     // private final DriveConstants driveConstants;
     // TODO: declare motor controller variables here
+     public static final double WHEEL_RADIUS = Units.inchesToMeters(3.0);
+  public static final double TRACK_WIDTH = Units.inchesToMeters(26.0);
+
+  // TODO: NON-SIM FEEDFORWARD GAINS MUST BE TUNED
+  // Consider using SysId routines defined in RobotContainer
+  private static final double KS = Constants.currentMode == Mode.SIM ? 0.0 : 0.0;
+  private static final double KV = Constants.currentMode == Mode.SIM ? 0.227 : 0.0;
+
+  private final DriveIO io;
+  private final DriveIOInputsAutoLogged inputs = new DriveIOInputsAutoLogged();
+  private final DifferentialDriveOdometry odometry =
+      new DifferentialDriveOdometry(new Rotation2d(), 0.0, 0.0);
+  private final DifferentialDriveKinematics kinematics =
+      new DifferentialDriveKinematics(TRACK_WIDTH);
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(KS, KV);
+  private final SysIdRoutine sysId;
+
+  /** Creates a new Drive. */
+  public Drive(DriveIO io) {
+    this.io = io;
+
+    // Configure AutoBuilder for PathPlanner
+    AutoBuilder.configureRamsete(
+        this::getPose,
+        this::setPose,
+        () ->
+            kinematics.toChassisSpeeds(
+                new DifferentialDriveWheelSpeeds(
+                    getLeftVelocityMetersPerSec(), getRightVelocityMetersPerSec())),
+        (speeds) -> {
+          var wheelSpeeds = kinematics.toWheelSpeeds(speeds);
+          driveVelocity(wheelSpeeds.leftMetersPerSecond, wheelSpeeds.rightMetersPerSecond);
+        },
+        new ReplanningConfig(),
+        () ->
+            DriverStation.getAlliance().isPresent()
+                && DriverStation.getAlliance().get() == Alliance.Red,
+        this);
+    Pathfinding.setPathfinder(new LocalADStarAK());
+    PathPlannerLogging.setLogActivePathCallback(
+        (activePath) -> {
+          Logger.recordOutput(
+              "Odometry/Trajectory", activePath.toArray(new Pose2d[activePath.size()]));
+        });
+    PathPlannerLogging.setLogTargetPoseCallback(
+        (targetPose) -> {
+          Logger.recordOutput("Odometry/TrajectorySetpoint", targetPose);
+        });
+
+    // Configure SysId
+    sysId =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                (state) -> Logger.recordOutput("Drive/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism(
+                (voltage) -> driveVolts(voltage.in(Volts), voltage.in(Volts)), null, this));
 
     public Drive() {
         // TODO: instantiate motor controllers here
