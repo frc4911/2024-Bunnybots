a878ee1fe8e8c211952c4c2d2efc621bc2d555d9
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-10-24 19:31:34 -0700

    Added Drive and DriveIO

--- BunnyBots/src/main/java/frc/robot/drive/DriveIO.java
+++ BunnyBots/src/main/java/frc/robot/drive/DriveIO.java
@@ -1,10 +1,32 @@
 package frc.robot.drive;
 
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public interface DriveIO {
-    public static class  DriveIOInputs {
-    public double drivePositionRad = 0.0;
-    public double driveVelocityRadPerSec = 0.0;
-    public double driveAppliedVolts = 0.0; 
-        
-    }
+  @AutoLog
+  public static class DriveIOInputs {
+    public double leftPositionRad = 0.0;
+    public double leftVelocityRadPerSec = 0.0;
+    public double leftAppliedVolts = 0.0;
+    public double[] leftCurrentAmps = new double[] {};
+
+    public double rightPositionRad = 0.0;
+    public double rightVelocityRadPerSec = 0.0;
+    public double rightAppliedVolts = 0.0;
+    public double[] rightCurrentAmps = new double[] {};
+
+    public Rotation2d gyroYaw = new Rotation2d();
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(DriveIOInputs inputs) {}
+
+  /** Run open loop at the specified voltage. */
+  public default void setVoltage(double leftVolts, double rightVolts) {}
+
+  /** Run closed loop at the specified velocity. */
+  public default void setVelocity(
+      double leftRadPerSec, double rightRadPerSec, double leftFFVolts, double rightFFVolts) {}
 }
