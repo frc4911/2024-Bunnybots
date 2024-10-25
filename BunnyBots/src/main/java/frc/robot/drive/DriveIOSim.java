db41274b5e1ed177e200f3d24145d9ad2e067288
script-js <134544514+script-js@users.noreply.github.com>
2024-10-24 19:32:05 -0700

    DriveIO SparkMax&Sim

--- BunnyBots/src/main/java/frc/robot/drive/DriveIOSim.java
+++ BunnyBots/src/main/java/frc/robot/drive/DriveIOSim.java
@@ -42,3 +42,69 @@ public class DriveIOSim implements DriveIO {
     }
   }
 }
+
+public class DriveIOSim implements DriveIO {
+  private static final double KP = 0.2;
+  private static final double KD = 0.0;
+  private DifferentialDrivetrainSim sim =
+      DifferentialDrivetrainSim.createKitbotSim(
+          KitbotMotor.kDualCIMPerSide, KitbotGearing.k10p71, KitbotWheelSize.kSixInch, null);
+
+  private double leftAppliedVolts = 0.0;
+  private double rightAppliedVolts = 0.0;
+  private boolean closedLoop = false;
+  private PIDController leftPID = new PIDController(KP, 0.0, KD);
+  private PIDController rightPID = new PIDController(KP, 0.0, KD);
+  private double leftFFVolts = 0.0;
+  private double rightFFVolts = 0.0;
+
+  @Override
+  public void updateInputs(DriveIOInputs inputs) {
+    if (closedLoop) {
+      leftAppliedVolts =
+          MathUtil.clamp(
+              leftPID.calculate(sim.getLeftVelocityMetersPerSecond() / Drive.WHEEL_RADIUS)
+                  + leftFFVolts,
+              -12.0,
+              12.0);
+      rightAppliedVolts =
+          MathUtil.clamp(
+              leftPID.calculate(sim.getRightVelocityMetersPerSecond() / Drive.WHEEL_RADIUS)
+                  + rightFFVolts,
+              -12.0,
+              12.0);
+      sim.setInputs(leftAppliedVolts, rightAppliedVolts);
+    }
+
+    sim.update(0.02);
+    inputs.leftPositionRad = sim.getLeftPositionMeters() / Drive.WHEEL_RADIUS;
+    inputs.leftVelocityRadPerSec = sim.getLeftVelocityMetersPerSecond() / Drive.WHEEL_RADIUS;
+    inputs.leftAppliedVolts = leftAppliedVolts;
+    inputs.leftCurrentAmps = new double[] {sim.getLeftCurrentDrawAmps()};
+
+    inputs.rightPositionRad = sim.getRightPositionMeters() / Drive.WHEEL_RADIUS;
+    inputs.rightVelocityRadPerSec = sim.getRightVelocityMetersPerSecond() / Drive.WHEEL_RADIUS;
+    inputs.rightAppliedVolts = rightAppliedVolts;
+    inputs.rightCurrentAmps = new double[] {sim.getRightCurrentDrawAmps()};
+
+    inputs.gyroYaw = sim.getHeading();
+  }
+
+  @Override
+  public void setVoltage(double leftVolts, double rightVolts) {
+    closedLoop = false;
+    leftAppliedVolts = MathUtil.clamp(leftVolts, -12.0, 12.0);
+    rightAppliedVolts = MathUtil.clamp(rightVolts, -12.0, 12.0);
+    sim.setInputs(leftAppliedVolts, rightAppliedVolts);
+  }
+
+  @Override
+  public void setVelocity(
+      double leftRadPerSec, double rightRadPerSec, double leftFFVolts, double rightFFVolts) {
+    closedLoop = true;
+    leftPID.setSetpoint(leftRadPerSec);
+    rightPID.setSetpoint(rightRadPerSec);
+    this.leftFFVolts = leftFFVolts;
+    this.rightFFVolts = rightFFVolts;
+  }
+}
