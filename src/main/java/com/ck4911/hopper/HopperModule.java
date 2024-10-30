e8df950ccf785ebadaeb87d4058f761719328456
Riley Brewer <117626660+Krylez@users.noreply.github.com>
2024-10-30 15:30:41 -0700

    Create HopperModule.java

---
+++ src/main/java/com/ck4911/hopper/HopperModule.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 FRC 4911
+// https://github.com/frc4911
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.ck4911.hopper;
+
+import com.ck4911.Constants.Mode;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Provider;
+
+@Module
+public interface HopperModule {
+
+  @Provides
+  public static HopperIO providesDriveIO(
+      Mode mode, Provider<HopperIOReal> realProvider, Provider<HopperIOSim> simProvider) {
+    switch (mode) {
+      case REAL:
+        return realProvider.get();
+      case SIM:
+        return simProvider.get();
+      default:
+        return new HopperIO() {};
+    }
+  }
+}
