0c8ea4fc8b1617de22c8f6a0c3d978c30c4a5fef
Riley Brewer <117626660+Krylez@users.noreply.github.com>
2024-10-30 15:43:04 -0700

    Create GrabberModule.java

---
+++ src/main/java/com/ck4911/grabber/GrabberModule.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 FRC 4911
+// https://github.com/frc4911
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.ck4911.grabber;
+
+import com.ck4911.Constants.Mode;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Provider;
+
+@Module
+public interface GrabberModule {
+
+  @Provides
+  public static GrabberIO providesGrabberIO(
+      Mode mode, Provider<GrabberIOReal> realProvider, Provider<CGrabberIOSim> simProvider) {
+    switch (mode) {
+      case REAL:
+        return realProvider.get();
+      case SIM:
+        return simProvider.get();
+      default:
+        return new GrabberIO() {};
+    }
+  }
+}
