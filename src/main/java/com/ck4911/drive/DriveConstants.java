04a91991b1bf59b377ecd788c2d0e2ca56609eb1
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-11-02 11:27:53 -0700

    Drive stufs

---
+++ src/main/java/com/ck4911/drive/DriveConstants.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2024 FRC 4911
+// https://github.com/frc4911
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.ck4911.drive;
+
+public final class DriveConstants {
+
+  private DriveConstants() {}
+
+  public static int frontRight = 1;
+  public static int backRight = 2;
+  public static int frontLeft = 3;
+  public static int backLeft = 4;
+}
